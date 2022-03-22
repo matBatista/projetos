6938277eee70d85a0b09818d3d0a41cab64fe30f
Matheus Batista <matheus.batistacs@gmail.com>
2022-03-22 06:47:50 -0300

    engine ejs
    npm install ejs --save

--- Node/primeiro/index.js
+++ Node/primeiro/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+//npm install ejs --save 
+//app.set para setar o app para usar a engine ejs
+app.set('view engine', 'ejs');
+
+
+app.get('/home', function(req,res){
+    res.render('../View/home')
+});
 
 app.get('/', function(req,res){
     res.json({name: "Matheus", idade: 25});
