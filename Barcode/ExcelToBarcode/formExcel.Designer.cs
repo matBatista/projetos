a2bfd5cb0edfb1fd18585b0f1a167ed5d1fcec83
Matheus Batista <matheus.batistacs@gmail.com>
2023-05-03 18:41:02 -0300

    generate a barcode for a list of products in a excel file.
    changes in libs and general code;

--- Barcode/ExcelToBarcode/formExcel.Designer.cs
+++ Barcode/ExcelToBarcode/formExcel.Designer.cs
@@ -29,67 +29,91 @@ namespace ExcelToBarcode
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtArquivo = new System.Windows.Forms.TextBox();
+            this.txtFileSource = new System.Windows.Forms.TextBox();
             this.btnIniciar = new System.Windows.Forms.Button();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.label1 = new System.Windows.Forms.Label();
-            this.btnSelecionar = new System.Windows.Forms.Button();
+            this.btnSelectOri = new System.Windows.Forms.Button();
+            this.txtFileDestiny = new System.Windows.Forms.TextBox();
+            this.btnSelectDest = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // txtArquivo
+            // txtFileSource
             // 
-            this.txtArquivo.Location = new System.Drawing.Point(12, 50);
-            this.txtArquivo.Name = "txtArquivo";
-            this.txtArquivo.Size = new System.Drawing.Size(153, 20);
-            this.txtArquivo.TabIndex = 0;
+            this.txtFileSource.Location = new System.Drawing.Point(12, 41);
+            this.txtFileSource.Name = "txtFileSource";
+            this.txtFileSource.Size = new System.Drawing.Size(226, 20);
+            this.txtFileSource.TabIndex = 0;
             // 
             // btnIniciar
             // 
-            this.btnIniciar.Location = new System.Drawing.Point(252, 47);
+            this.btnIniciar.Location = new System.Drawing.Point(12, 141);
             this.btnIniciar.Name = "btnIniciar";
-            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.Size = new System.Drawing.Size(315, 23);
             this.btnIniciar.TabIndex = 1;
             this.btnIniciar.Text = "START";
             this.btnIniciar.UseVisualStyleBackColor = true;
             this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
             // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 96);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(315, 23);
-            this.progressBar1.TabIndex = 2;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Location = new System.Drawing.Point(12, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(84, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Informar Arquivo";
             // 
-            // btnSelecionar
+            // btnSelectOri
             // 
-            this.btnSelecionar.Location = new System.Drawing.Point(171, 47);
-            this.btnSelecionar.Name = "btnSelecionar";
-            this.btnSelecionar.Size = new System.Drawing.Size(75, 23);
-            this.btnSelecionar.TabIndex = 4;
-            this.btnSelecionar.Text = "SELECT";
-            this.btnSelecionar.UseVisualStyleBackColor = true;
-            this.btnSelecionar.Click += new System.EventHandler(this.btnSelecionar_Click);
+            this.btnSelectOri.Location = new System.Drawing.Point(254, 41);
+            this.btnSelectOri.Name = "btnSelectOri";
+            this.btnSelectOri.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectOri.TabIndex = 4;
+            this.btnSelectOri.Text = "SELECT";
+            this.btnSelectOri.UseVisualStyleBackColor = true;
+            this.btnSelectOri.Click += new System.EventHandler(this.btnSelecionar_Click);
+            // 
+            // txtFileDestiny
+            // 
+            this.txtFileDestiny.Location = new System.Drawing.Point(12, 90);
+            this.txtFileDestiny.Name = "txtFileDestiny";
+            this.txtFileDestiny.Size = new System.Drawing.Size(226, 20);
+            this.txtFileDestiny.TabIndex = 5;
+            // 
+            // btnSelectDest
+            // 
+            this.btnSelectDest.Location = new System.Drawing.Point(252, 90);
+            this.btnSelectDest.Name = "btnSelectDest";
+            this.btnSelectDest.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectDest.TabIndex = 6;
+            this.btnSelectDest.Text = "SELECT";
+            this.btnSelectDest.UseVisualStyleBackColor = true;
+            this.btnSelectDest.Click += new System.EventHandler(this.btnSelectDestino_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Indicar local de destino";
             // 
             // formExcel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(341, 135);
-            this.Controls.Add(this.btnSelecionar);
+            this.ClientSize = new System.Drawing.Size(341, 185);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSelectDest);
+            this.Controls.Add(this.txtFileDestiny);
+            this.Controls.Add(this.btnSelectOri);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.btnIniciar);
-            this.Controls.Add(this.txtArquivo);
+            this.Controls.Add(this.txtFileSource);
             this.Name = "formExcel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Excel to Barcode PDF";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -98,11 +122,13 @@ namespace ExcelToBarcode
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtArquivo;
+        private System.Windows.Forms.TextBox txtFileSource;
         private System.Windows.Forms.Button btnIniciar;
-        private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnSelecionar;
+        private System.Windows.Forms.Button btnSelectOri;
+        private System.Windows.Forms.TextBox txtFileDestiny;
+        private System.Windows.Forms.Button btnSelectDest;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Barcode/ExcelToBarcode/formExcel.cs
+++ Barcode/ExcelToBarcode/formExcel.cs
@@ -1,125 +1,277 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using GemBox.Document;
-using GemBox.Document.Tables;
-using ZXing;
-using ZXing.Common;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Drawing.Imaging;
+using A = DocumentFormat.OpenXml.Drawing;
+using DW = DocumentFormat.OpenXml.Drawing.Wordprocessing;
+using PIC = DocumentFormat.OpenXml.Drawing.Pictures;
+using Microsoft.Office.Interop.Excel;
+using Drawing = DocumentFormat.OpenXml.Wordprocessing.Drawing;
+using Application = Microsoft.Office.Interop.Excel.Application;
+using BarcodeLib;
+using Font = System.Drawing.Font;
+using Color = System.Drawing.Color;
 
 namespace ExcelToBarcode
 {
     public partial class formExcel : Form
     {
+        public string pathInitial = "";
+        public string pathDest = "";
         public formExcel()
         {
             InitializeComponent();
 
-            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+            //ComponentInfo.SetLicense("FREE-LIMITED-KEY");
         }
 
         private void btnSelecionar_Click(object sender, EventArgs e)
         {
-            
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            if (ofd.FileName != "")
+            {
+                pathInitial = ofd.FileName;
+            }
+            else
+            {
+                pathInitial = "";
+            }
+
+            txtFileSource.Text = pathInitial;
+        }
+        private void btnSelectDestino_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            if (fbd.SelectedPath.ToString() != "")
+            {
+                pathDest = fbd.SelectedPath.ToString();
+            }
+            else
+            {
+                pathDest = "";
+            }
+
+            txtFileDestiny.Text = pathDest;
         }
 
         private void btnIniciar_Click(object sender, EventArgs e)
         {
+            string filedestiny = txtFileDestiny.Text;
+            string fileorigin = txtFileSource.Text;
 
-            DocumentModel document = new DocumentModel();
-
-            Section section = new Section(document);
-            document.Sections.Add(section);
-
-            BarcodeWriter writer = new BarcodeWriter
+            if (fileorigin != "")
             {
-                Format = BarcodeFormat.CODE_128, // Set the barcode format
-                Options = new EncodingOptions // Set the encoding options
+                if (!File.Exists(fileorigin))
                 {
-                    Height = 100,
-                    Width = 300,
-                    Margin = 5
-                }
-            };
-
-            List<string> codigos = new List<string>();
-
-            int x = 0;
-            while(x < 16)
-            {
-                codigos.Add("Item Teste " + x.ToString().PadLeft(3));
-                x++;
-            }
-            /*
-            for (int i = 0; i < codigos.Count; i++)
-            {
-                Paragraph paragraph = new Paragraph(document);
-
-                //Run barcodeRun = new Run(document, codigos[i]);
-                //paragraph.Inlines.Add(barcodeRun);
-
-                Bitmap barcodeBitmap = writer.Write(codigos[i]);
-                MemoryStream stream = new MemoryStream();
-                barcodeBitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
-
-                Picture barcodePicture = new Picture(document, stream);
-
-                paragraph.Inlines.Add(barcodePicture);
-
-                section.Blocks.Add(paragraph);
-            }*/
-
-            // Set the number of columns
-            int columns = 2;
-
-            // Calculate the number of rows needed
-            int rows = (int)Math.Ceiling((double)codigos.Count / columns);
-
-            // Create a table with the specified number of rows and columns
-            Table table = new Table(document);
-            for (int i = 0; i < rows; i++)
-            {
-                table.Rows.Add(new TableRow(document));
-                for (int j = 0; j < columns; j++)
-                {
-                    table.Rows[i].Cells.Add(new TableCell(document));
+                    MessageBox.Show("Erro na leitura do arquivo selecionado.");
+                    return;
                 }
             }
 
-            // Add a barcode to each cell of the table
-            for (int i = 0; i < codigos.Count; i++)
+            if (filedestiny != "")
             {
-                Bitmap barcodeBitmap = writer.Write(codigos[i]);
-                MemoryStream stream = new MemoryStream();
-                barcodeBitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
-
-                Picture barcodePicture = new Picture(document, stream);
-
-                int row = i / columns;
-                int column = i % columns;
-
-                Paragraph paragraph = new Paragraph(document, barcodePicture);
-                table.Rows[row].Cells[column].Blocks.Add(paragraph);
+                if (!Directory.Exists(filedestiny))
+                {
+                    Directory.CreateDirectory(filedestiny);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selecionar um diretório de destino.");
+                return;
             }
 
-            // Add the table to the document
-            section.Blocks.Add(table);
+            List<Produto> listProdutos = new List<Produto>();
 
 
-            string filePath = "D:\\Matheus\\Trabalho 2\\Barcode\\barcodes.docx";
-            document.Save(filePath);
+            // Open Excel application and workbook
+            Application excel = new Application();
+            Workbook workbook = excel.Workbooks.Open(fileorigin);
 
-            if(MessageBox.Show("Arquivo gerado deseja abrir?", "Barcode", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            // Select the worksheet you want to read data from
+            Worksheet worksheet = (Worksheet)workbook.Worksheets["Planilha1"];
+
+            // Get the range of data in columns A and B
+            Range range = worksheet.UsedRange.Columns["A:B"];
+
+            // Iterate through each cell in the column and print its value
+            foreach (Range row in range.Rows)
             {
-                Process.Start(filePath);
+                Produto prod = new Produto();
+                prod.codigo = row.Cells[1,1].Value2.ToString();
+                prod.descricao = row.Cells[1,2].Value2.ToString();
+
+                listProdutos.Add(prod);
             }
+
+            // Close workbook and Excel application
+            workbook.Close();
+            excel.Quit();
+        
+            string result = ProcessDocument(filedestiny + "\\barcorde.docx", listProdutos);
+
+            if (File.Exists(result))
+            {
+                if (MessageBox.Show("Arquivo gerado deseja abrir?", "Barcode", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Process.Start(result);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error ao gerar arquivo" + Environment.NewLine + result, "Barcode", MessageBoxButtons.OK);
+            }
+        }
+
+        public string ProcessDocument(string file, List<Produto> listProdutos)
+        {
+            using (WordprocessingDocument wordDocument = WordprocessingDocument.Create(file, WordprocessingDocumentType.Document))
+            {
+                MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+                mainPart.Document = new Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+
+                // Set up three columns and adjust the bottom margin
+                SectionProperties sectionProperties = new SectionProperties();
+                
+                PageSize pageSize = new PageSize() { Width = (UInt32Value)12240U, Height = (UInt32Value)15840U }; // Page size (in twips)
+                PageMargin pageMargin = new PageMargin() { Top = 1440,
+                    Right = (UInt32Value)720U, 
+                    Bottom = 780,
+                    Left = (UInt32Value)720U, 
+                    Header = (UInt32Value)720U, 
+                    Footer = (UInt32Value)720U, 
+                    Gutter = (UInt32Value)0U }; // Page margins (in twips)
+
+                sectionProperties.Append(pageSize);
+                sectionProperties.Append(pageMargin);
+                
+                body.Append(sectionProperties);
+
+                // Set the number of columns
+                int columns = 4;
+
+                // Calculate the number of rows needed
+                int rows = (int)Math.Ceiling((double)listProdutos.Count / columns);
+
+                int prods = 0;
+                // Create a table with the specified number of rows and columns
+                Table table = new Table();
+
+                //Table table = new Table(new TableProperties(
+                //    new marg,
+                //    new TableWidth() { Type = TableWidthUnitValues.Pct, Width = "100" },
+                //    new TableLayout() { Type = TableLayoutValues.Fixed }));
+
+                for (int i = 0; i < rows; i++)
+                {
+                    TableRow row = new TableRow();
+                    for (int j = 0; j < columns; j++)
+                    {
+                        TableCell cell = new TableCell(
+                            new TableCellProperties(new TableCellWidth() { Type = TableWidthUnitValues.Auto }));
+
+                        // Set the margins for the cell
+                        TableCellMargin margin = new TableCellMargin();
+                        margin.TopMargin = new TopMargin() { Width = "0.1in" };
+                        margin.BottomMargin = new BottomMargin() { Width = "0.2in" };
+                        margin.LeftMargin = new LeftMargin() { Width = "0.3in" };
+                        margin.RightMargin = new RightMargin() { Width = "0.3in" };
+
+                        cell.Append(new TableCellProperties(margin));
+
+                        Paragraph paragraph = new Paragraph();
+                        Run run = new Run();
+                        paragraph.Append(run);
+
+                        Produto prod = listProdutos[prods];
+                        DW.Inline inline = DrawInlineObject(mainPart, prod);
+
+                        run.Append(new Drawing(inline));
+
+                        Run line = new Run(new Break());
+                        paragraph.Append(line);
+
+                        Run runtext = new Run(new Text(prod.descricao.ToUpper()));
+                        runtext.RunProperties = new RunProperties(
+                            new RunFonts() { Ascii = "Arial" },
+                            new FontSize() { Val = "14" },
+                            new Justification() { Val = JustificationValues.Left });
+
+                        paragraph.Append(runtext);
+
+                        prods++;
+
+                        row.Append(cell);
+                        cell.Append(paragraph);
+                    }
+                    table.Append(row);
+                }
+                // Add the table to the body of the document
+                body.Append(table);
+                mainPart.Document.Save();
+            }
+
+            return file;
+        }
+
+        public DW.Inline DrawInlineObject(MainDocumentPart mainPart, Produto prod)
+        {
+            string barcodeValue = prod.codigo ;
+            string description = prod.descricao;
+
+            Barcode b = new Barcode();
+            Image barcodeImage = b.Encode(BarcodeLib.TYPE.CODE128, barcodeValue, Color.Black, Color.White, 300, 100);
+
+            Graphics graphics = Graphics.FromImage(barcodeImage);
+            graphics.DrawString(description, new Font("Arial", 10), Brushes.Black, new PointF(0, 100));
+
+            MemoryStream stream = new MemoryStream();
+            barcodeImage.Save(stream, ImageFormat.Png);
+            byte[] imageBytes = stream.ToArray();
+
+            ImagePart imagePart = mainPart.AddImagePart(ImagePartType.Jpeg);
+            stream.Position = 0;
+            imagePart.FeedData(stream);
+
+            string imageId = mainPart.GetIdOfPart(imagePart);
+
+            DW.Inline inline = new DW.Inline(
+                new DW.Extent() { Cx = 990000L, Cy = 792000L },
+                new DW.EffectExtent() { LeftEdge = 0L, TopEdge = 0L, RightEdge = 0L, BottomEdge = 0L },
+                new DW.DocProperties() { Id = (UInt32Value)1U, Name = "Picture 1" },
+                new DW.NonVisualGraphicFrameDrawingProperties(new A.GraphicFrameLocks() { NoChangeAspect = true }),
+                new A.Graphic(new A.GraphicData(new PIC.Picture(
+                    new PIC.NonVisualPictureProperties(
+                        new PIC.NonVisualDrawingProperties() { Id = (UInt32Value)0U, Name = "New Bitmap Image.jpg" },
+                        new PIC.NonVisualPictureDrawingProperties()),
+                    new PIC.BlipFill(
+                        new A.Blip(new A.BlipExtensionList(new A.BlipExtension() { Uri = "{28A0092B-C50C-407E-A947-70E740481C1C}" })) { Embed = imageId, CompressionState = A.BlipCompressionValues.Print },
+                        new A.Stretch(new A.FillRectangle())),
+                    new PIC.ShapeProperties(
+                        new A.Transform2D(new A.Offset() { X = 0L, Y = 0L },
+                            new A.Extents() { Cx = 990000L, Cy = 792000L }),
+                        new A.PresetGeometry(new A.AdjustValueList()) { Preset = A.ShapeTypeValues.Rectangle }))
+                )
+                { Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture" })
+            );
+
+
+            return inline;
+        }
+        public class Produto
+        {
+            public string codigo { get; set; }
+            public string descricao { get; set; }
         }
     }
 }
